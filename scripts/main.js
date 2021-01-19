fe02615cdcb9533ddf3d606c1a84a10c98e59dad
Gustavo <gustavo7dias@gmail.com>
2021-01-19 19:40:07 -0300

    Refactor code

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,50 @@
-function evaluate() {
-    displayResult.innerText = eval(calculation);
-    clearAllFunc()
-}
+const operators = document.getElementsByClassName('operator');
+const dot = document.getElementById('dot');
+const clears = document.getElementsByClassName('clear')
+const displayCalculation = document.getElementById('calculation');
+const displayResult = document.getElementById('result');
+
+let calculation = '';
+
+window.document.addEventListener('click', event => {
+    if (!event.target.classList.contains('num-button')) return;
+    calculation += event.target.innerText;
+    printCalculation(calculation);
+})
 
 function printCalculation(calcValue) {
     displayCalculation.value = calcValue;
 }
 
-//backspace
+window.document.addEventListener('click', event => {
+    if (!event.target.classList.contains('operator')) return;
+    addOperator(event.target);
+})
+
+function addOperator(eventTarget) {
+    if (calculation === '' && eventTarget.id === 'sub') {
+        calculation += '-';
+        printCalculation(calculation);
+    } else if (calculation !== '') {
+        let lastChar = calculation.charAt(calculation.length - 1);
+        let ops = '+-*/';
+        if (!ops.includes(lastChar)) {
+            calculation += eventTarget.innerText;
+            printCalculation(calculation);
+        }
+    }
+}
+
+operators.equal.addEventListener('click', evaluate);
+clears.clearAll.addEventListener('click', clearAllFunc);
+clears.clear.addEventListener('click', clearFunc);
+dot.addEventListener('click', addDot);
+
+function evaluate() {
+    displayResult.innerText = eval(calculation);
+    clearAllFunc()
+}
+
 function clearFunc() {
     calculation = calculation.substr(0, calculation.length - 1);
     printCalculation(calculation);
@@ -18,69 +55,15 @@ function clearAllFunc() {
     displayCalculation.value = '';
 }
 
-function addOperator(indexIdButton) {
-    //add only substract operator in start of calculation
-    if (calculation === '' && operators[indexIdButton].id === 'sub') {
-        calculation += '-';
-        printCalculation(calculation);
-    } else if (calculation !== '' && calculation.charAt(calculation.length - 1) !== operators[indexIdButton].innerText) {
-        calculation += operators[indexIdButton].innerText;
-        printCalculation(calculation);
-    }
-}
-
 function addDot() {
-    if (calculation.charAt(calculation.length - 1) !== '.') {
-        if (calculation !== '' && Number(calculation.charAt(calculation.length - 1)) !== NaN) {
-            calculation += '.';
-            printCalculation(calculation);
+    let lastChar = calculation.charAt(calculation.length - 1);
+    let ops = '+-*/';
+    if (lastChar !== '.') {
+        if (calculation !== '' && parseInt(lastChar) !== NaN) {
+            if (!ops.includes(lastChar)) {
+                calculation += '.';
+                printCalculation(calculation);
+            }
         }
     }
 }
-
-// if the last char at calculation is an operation or dor
-// fix the bug of sequence of diferent operators and dots
-// function isOperatorOrDot() {}
-
-const numbers = document.getElementsByClassName('num-button');
-const operators = document.getElementsByClassName('operator');
-const dot = document.getElementById('dot');
-const clears = document.getElementsByClassName('clear')
-const displayCalculation = document.getElementById('calculation');
-const displayResult = document.getElementById('result');
-
-//Variable to put the calculation
-let calculation = '';
-
-//Add a EventListener, put numbers into calculation variable
-for (let i = 0; i < 10; i++) {
-    numbers[i].addEventListener('click', function () {
-        calculation += numbers[i].innerText;
-        printCalculation(calculation);
-    })
-}
-
-for (let i = 0; i < operators.length; i++) {
-    if (operators[i].id === 'add') {
-        operators[i].addEventListener('click', function () {
-            addOperator(i);
-        });
-    } else if (operators[i].id === 'sub') {
-        operators[i].addEventListener('click', function () {
-            addOperator(i);
-        });
-    } else if (operators[i].id === 'mul') {
-        operators[i].addEventListener('click', function () {
-            addOperator(i);
-        });
-    } else if (operators[i].id === 'div') {
-        operators[i].addEventListener('click', function () {
-            addOperator(i);
-        });
-    }
-}
-
-operators.equal.addEventListener('click', evaluate);
-clears.clearAll.addEventListener('click', clearAllFunc);
-clears.clear.addEventListener('click', clearFunc);
-dot.addEventListener('click', addDot);
